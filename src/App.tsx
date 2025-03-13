287058e6a6bde0c52636ba6855f5b763f0ef4bf3
Omer Kulaksiz <omer.faruk.kulaksiz@outlook.com>
2025-03-13 11:54:49 +0100

    changed typos

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,45 @@
-import React, { useRef, useEffect } from 'react';
+// import React, { useRef, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { useLoader } from '@react-three/fiber';
-import { useFrame } from '@react-three/fiber';
-import * as THREE from 'three';
+// import { useFrame } from '@react-three/fiber';
+// import * as THREE from 'three';
 import './App.css';
 
 
-function Torus() {
-    const mesh = useRef<THREE.Mesh>(null!);
+// function Torus() {
+//     const mesh = useRef<THREE.Mesh>(null!);
+//
+//     useFrame(() => {
+//         if (mesh.current) {
+//             mesh.current.rotation.x += 0.01;
+//             mesh.current.rotation.y += 0.005;
+//             mesh.current.rotation.z += 0.01;
+//         }
+//     });
+//
+//     return (
+//         <mesh ref={mesh} position={[0, 10, 0]}>
+//             <torusGeometry args={[7, 2, 16, 90]} />
+//             <meshStandardMaterial color="#FF6347" wireframe={false} />
+//         </mesh>
+//     );
+// }
 
-    useFrame(() => {
-        if (mesh.current) {
-            mesh.current.rotation.x += 0.01;
-            mesh.current.rotation.y += 0.005;
-            mesh.current.rotation.z += 0.01;
-        }
-    });
-
-    return (
-        <mesh ref={mesh} position={[0, 10, 0]}>
-            <torusGeometry args={[7, 2, 16, 90]} />
-            <meshStandardMaterial color="#FF6347" wireframe={false} />
-        </mesh>
-    );
-}
-
-function Plane() {
-    return (
-        <mesh position={[-10, -10, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-            <planeGeometry args={[75, 75, 10, 10]} />
-            <meshStandardMaterial
-                color="white"
-                side={THREE.DoubleSide}
-                metalness={0.5}
-                roughness={0.7}
-            />
-        </mesh>
-    );
-}
+// function Plane() {
+//     return (
+//         <mesh position={[-10, -10, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+//             <planeGeometry args={[75, 75, 10, 10]} />
+//             <meshStandardMaterial
+//                 color="white"
+//                 side={THREE.DoubleSide}
+//                 metalness={0.5}
+//                 roughness={0.7}
+//             />
+//         </mesh>
+//     );
+// }
 
 function OfficeModel() {
     const gltf = useLoader(GLTFLoader, '/office.glb');
