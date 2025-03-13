64811912a5f3aef06c9a6ae021e3a220737c1baf
Omer Kulaksiz <omer.faruk.kulaksiz@outlook.com>
2025-03-13 14:23:13 +0100

    quick fix 2

--- src/model.tsx
+++ src/model.tsx
@@ -1,14 +1,14 @@
-import {Environment, useGLTF, useHelper} from "@react-three/drei";
-import {useRef} from "react";
-import {DirectionalLightHelper, SpotLightHelper} from "three";
+import {Environment, useGLTF,} from "@react-three/drei";
+// import {useRef} from "react";
+// import {DirectionalLightHelper, SpotLightHelper} from "three";
 
 export default function Model() {
     const model = useGLTF("office.glb");
-    // @ts-ignore
-    const lightRef = useRef();
-    const amRef = useRef(null);
-    // @ts-ignore
-    useHelper(lightRef, DirectionalLightHelper, 3, "red");
+    // // @ts-ignore
+    // const lightRef = useRef();
+    // const amRef = useRef(null);
+    // // @ts-ignore
+    // useHelper(lightRef, DirectionalLightHelper, 3, "red");
     return(
         <>
             <Environment preset={"night"}></Environment>
